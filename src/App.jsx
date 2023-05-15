ba3ce0c89f7378c9b2bed31f286a4b333a90efdd
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-05-15 22:52:02 +1000

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
-
 import Die from "./components/Die";
+import { useState } from "react";
+import {nanoid} from "nanoid"
+
 
 export default function App() {
-  return(
-    <main className="w-96 h-96  bg-white
-     rounded-md flex justify-center items-center">
-      <div className="grid grid-cols-5 grid-rows-2 gap-x-5 gap-y-6">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="7" />
-        <Die value="8" />
-        <Die value="9" />
-        <Die value="1" />
-        <Die value="1" />
+  const [randomDice, setRandomDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({ 
+        value: Math.floor(Math.random() * 6) + 1, 
+        isHeld: false,
+        id: nanoid()
+      });
+    }
+    console.log(newDice)
+    return newDice;
+  }
+
+  function rollDice() {
+    setRandomDice(allNewDice());
+  }
+
+  const diceElements = randomDice.map((die) => {
+    return <Die value={die.value} key={die.id} isHeld={die.isHeld}/>;
+  });
+
+  return (
+    <main
+      className="w-96 h-96  bg-white
+     rounded-md flex justify-center items-center"
+    >
+      <div className="flex flex-col items-center space-y-14">
+        <div className="grid grid-cols-5 grid-rows-2 gap-x-5 gap-y-6">
+          {diceElements}
+        </div>
+        <button
+          className="bg-[#5035FF] w-24 h-9 text-white rounded shadow-lg"
+          onClick={rollDice}
+        >
+          Roll
+        </button>
       </div>
     </main>
-  )
+  );
 }
-
-
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,13 +1,10 @@
-import PropTypes from "prop-types";
 
 export default function Die(props) {
+  const styles = props.isHeld ? "bg-[#59E391]" : "bg-white"
+  
   return (
-    <div className="w-8 h-9 shadow-md rounded flex justify-center items-center">
+    <div className={`w-10 h-12 shadow-md rounded flex justify-center items-center ${styles}`}>
       <h2 className="font-bold text-xl">{props.value}</h2>
     </div>
   );
 }
-
-Die.propTypes = {
-  value: PropTypes.string.isRequired,
-};
