1cf07d88475cc9db682071754160dcdfc2b3b684
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-05-16 19:07:47 +1000

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,76 @@
 import Die from "./components/Die";
-import { useState } from "react";
-import {nanoid} from "nanoid"
-
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 
 export default function App() {
   const [randomDice, setRandomDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    // TODO fix the logic for this
+    randomDice.forEach((die) => {
+      if (die.isHeld && die.value) {
+        setTenzies(true);
+        console.log("You won!");
+      }
+    });
+  }, [randomDice]);
+
+  function generateNewDie() {
+    return {
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
+  function holdDice(id) {
+    setRandomDice((prevDice) =>
+      prevDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  function rollDice() {
+    setRandomDice((oldDice) =>
+      oldDice.map((existingDice) => {
+        return existingDice.isHeld ? existingDice : generateNewDie();
+      })
+    );
+  }
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({ 
-        value: Math.floor(Math.random() * 6) + 1, 
-        isHeld: false,
-        id: nanoid()
-      });
+      newDice.push(generateNewDie());
     }
-    console.log(newDice)
     return newDice;
   }
 
-  function rollDice() {
-    setRandomDice(allNewDice());
-  }
-
   const diceElements = randomDice.map((die) => {
-    return <Die value={die.value} key={die.id} isHeld={die.isHeld}/>;
+    return (
+      <Die
+        holdDice={holdDice}
+        id={die.id}
+        value={die.value}
+        key={die.id}
+        isHeld={die.isHeld}
+      />
+    );
   });
 
   return (
     <main
       className="w-96 h-96  bg-white
-     rounded-md flex justify-center items-center"
+     rounded-md flex flex-col justify-center items-center"
     >
-      <div className="flex flex-col items-center space-y-14">
+      <h1 className="text-2xl font-bold">Tenzies</h1>
+      <p className="text-center text-sm max-w-xs">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="flex flex-col items-center space-y-14 mt-5">
         <div className="grid grid-cols-5 grid-rows-2 gap-x-5 gap-y-6">
           {diceElements}
         </div>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -2,8 +2,11 @@
 export default function Die(props) {
   const styles = props.isHeld ? "bg-[#59E391]" : "bg-white"
   
+
+
   return (
-    <div className={`w-10 h-12 shadow-md rounded flex justify-center items-center ${styles}`}>
+    <div onClick={() => props.holdDice(props.id)} className={`w-10 h-12 shadow-md rounded flex justify-center items-center text-black
+     ${styles}`}>
       <h2 className="font-bold text-xl">{props.value}</h2>
     </div>
   );
